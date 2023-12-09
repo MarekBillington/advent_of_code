38a88a1d32b1c6025946f17842bffcdc23a2c193
MarekBillington <MarekBillington@users.noreply.github.com>
2023-12-09 00:40:13 +0000

    both parts completed

--- 2023/php/day_1/day_1.php
+++ 2023/php/day_1/day_1.php
@@ -1,21 +1,62 @@
 <?php
 
-$raw = file_get_contents(__DIR__.'/data.txt');
+function process($part) {
+    $raw = file_get_contents(__DIR__.'/data.txt');
 
-$split = explode("\n", $raw);
+    $split = explode("\n", $raw);
 
-$sum = 0;
-foreach ($split as $value) {
-    $matches = [];
-    preg_match_all("(\d)", $value, $matches);
+    $sum = 0;
 
-    $m = $matches[0];
-    
-    $f = $m[0];
-    $e = $m[array_key_last($m)];
+    if (1 == $part) {
+        foreach ($split as $value) {
+            $matches = [];
+            preg_match_all("(\d)", $value, $matches);
+        
+            $m = $matches[0];
+            $f = $m[0];
+            $e = $m[array_key_last($m)];
+        
+            $s = intval($f.$e);
+            $sum += $s;
+        }
+    } else if (2 == $part) {
+        foreach ($split as $value) {
+            $m = [];
+            $digits = ['one' => 1,'two' => 2,'three' => 3,'four' =>4,'five'=>5,'six'=>6,'seven'=>7,'eight'=>8,'nine'=>9];
 
-    $s = intval($f.$e);
-    $sum += $s;
+            // check over each option and push into array with index as key...
+            foreach ($digits as $k => $v) {
+                // look forwards
+                $posV = strpos($value, $v);
+                $posK = strpos($value, $k);
+                if (false !== $posV)
+                    $m[$posV] = $v;
+                if (false !== $posK)
+                    $m[$posK] = $v;
+
+                // look backwards
+                $posV = strrpos($value, $v);
+                $posK = strrpos($value, $k);
+                if (false !== $posV)
+                    $m[$posV] = $v;
+                if (false !== $posK)
+                    $m[$posK] = $v;
+                    
+            }
+        
+            ksort($m);
+            print_r($m);
+            $f = $m[array_key_first($m)];
+            $e = $m[array_key_last($m)];
+        
+            $s = intval($f.$e);
+            print_r($s."\n");
+            $sum += $s;
+        }
+    }
+
+    return $sum;
 }
 
-echo $sum;
+echo "part 1: ".process(1)."\n";
+echo "part 2: ".process(2)."\n";
